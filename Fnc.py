5c94056214545ee0cfab85139d40f5ff37f71f76
Yusuf-Hassan9 <yusuf.hassan@bcestudent.com>
2024-11-19 22:10:30 +0000

    Add files via upload

---
+++ Fnc.py
@@ -0,0 +1,39 @@
+
+# The Message Delay Function
+import sys,time
+
+def sprint(str):
+   for c in str + '\n':
+     sys.stdout.write(c)
+     sys.stdout.flush()
+     time.sleep(0.0000009)
+
+# Error Validation
+def get_valid_input(prompt, options):
+    """Helper function to get a valid input from the player."""
+    while True:
+        choice = input(prompt).lower().strip()
+        if choice in options:
+            return choice
+        elif choice == '':
+            sprint("You didn't enter anything. Please choose a valid option!\n")
+        else:
+            sprint("Invalid choice. Please choose a valid option!\n")
+
+
+# Dialogue names (constants)
+ai = "Narrator:"
+npc = "Morpheus:"
+hman = "Matrix Henchman:"
+hmanb = "Matrix Henchman Boss:"
+mea = "Matrix Elite Agents:"
+mg = "Matrix Guard:"
+am = "Agent Myth:"
+
+# Press enter to continue 
+def enter():
+    input("Press ENTER to Continue.\n")
+   #sprint("Continuing... *60")
+
+
+       
